cdb5902ff11d0f23cf16ffd113e935c83e346649
ramirex <ramirex@gmail.com>
2020-02-13 12:17:20 -0400

    cambiar los nombres de los estados

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as fromFiltro from '../../filter/filter.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  filtrosValidos: fromFiltro.filtrosValidos [] = ['todos', 'completados', 'pendientes'];
+  filtroActual: fromFiltro.filtrosValidos;
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+
+    this.store.subscribe( state => {
+      this.filtroActual = state.filtro;
+    });
+  }
+
+  cambiarFiltro(nuevoFiltro: fromFiltro.filtrosValidos) {
+    const accion = new fromFiltro.SetFiltroAction(nuevoFiltro);
+    this.store.dispatch(accion);
   }
 
 }
